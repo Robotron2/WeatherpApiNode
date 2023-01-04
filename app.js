9b0779f6211665b3f845f182e86cedd01e99adcf
Theophilus Adeleke <theophilusadesola002@gmail.com>
2023-01-04 20:31:50 +0100

    Response can now be fetched from the API

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require("express")
-const bodyParser = require("body-parser")
-const { urlencoded } = require("body-parser")
+const https = require("https")
+// const bodyParser = require("body-parser")
 
 const app = express()
-app.use(bodyParser, urlencoded({ extended: true }))
+// app.use(bodyParser, urlencoded({ extended: true }))
 
 app.get("/", function (req, res) {
+	const url = `https://api.openweathermap.org/data/2.5/weather?q=lagos&appid=${process.env.API_KEY}&units=metric`
+	https.get(url, (response) => {
+		console.log(response)
+	})
+
 	res.sendFile(`${__dirname}/index.html`)
 })
 
