ca2dcf1c56dfe06466647d78ced329e663036bf7
Theophilus Adeleke <theophilusadesola002@gmail.com>
2023-01-05 16:19:36 +0100

    We can get dynamic data from the userinput and catch the input then use it to fetch an API then send the data to the browser using Express and Node

--- app.js
+++ app.js
@@ -2,27 +2,35 @@ require("dotenv").config()
 
 const express = require("express")
 const https = require("https")
-// const bodyParser = require("body-parser")
+const bodyParser = require("body-parser")
+const { request } = require("http")
 
 const app = express()
-// app.use(bodyParser, urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get("/", function (req, res) {
-	const url = `https://api.openweathermap.org/data/2.5/weather?q=lagos&appid=${process.env.API_KEY}&units=metric`
-	https.get(url, (response) => {
-		console.log(response.statusCode)
-		response.on("data", (data) => {
-			const weatherData = JSON.parse(data)
-			const icon = weatherData.weather[0].icon
-			const imageURL = `https://openweathermap.org/img/wn/${icon}@2x.png`
-
-			res.write(`<h1> The current weather description is ${weatherData.weather[0].description} </h1>`)
-			res.write(`<h1>The temperature in Lagos is ${weatherData.main.temp} degree celsius</h1>`)
-			res.write("<img src=" + imageURL + ">")
-			res.send()
-		})
-	})
+	res.sendFile(`${__dirname}/index.html`)
 })
+
+app.post("/", (req, res) => {
+	const query = req.body.city
+})
+
+// 	const unit = ""
+// 	const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${process.env.API_KEY}&units=${unit}`
+// 	https.get(url, (response) => {
+// 		console.log(response.statusCode)
+// 		response.on("data", (data) => {
+// 			const weatherData = JSON.parse(data)
+// 			const icon = weatherData.weather[0].icon
+// 			const imageURL = `https://openweathermap.org/img/wn/${icon}@2x.png`
+
+// 			res.write(`<h1> The current weather description is ${weatherData.weather[0].description} </h1>`)
+// 			res.write(`<h1>The temperature in Lagos is ${weatherData.main.temp} degree celsius</h1>`)
+// 			res.write("<img src=" + imageURL + ">")
+// 			res.send()
+// 		})
+// 	})
 
 app.listen(3000, () => {
 	console.log("App is running on port 3000")
